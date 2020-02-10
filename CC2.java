05ecbfd62d3ca73dbf3dab0bcb702a58715809fb
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-02-10 15:16:12 -0500

    Changed the program a bit, now it is almost done

--- CC2.java
+++ CC2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 class CC2{
     static Scanner sc = new Scanner(System.in);
@@ -14,17 +14,37 @@ class CC2{
         int finishPoss, finishSize;
         int tWater = 0;
         int counter = 0;
+        int between = 0;
+        boolean greaterThanOne = false;
         for(int i = 0; i < arr.length; i++){
+            if(counter > 0 && greaterThanOne){
+                if(arr[i] < startingSize){
+                    between += arr[i];
+                }
+                if(arr[i] >= startingSize){
+                    finishPoss = i;
+                    finishSize = arr[i];
+                    tWater += ((((finishPoss-1) - startingPoss)*startingSize)- between);
+                    counter = 0;
+                    greaterThanOne = false;
+                }
+            }
+            
+            if(counter > 0 && !greaterThanOne && arr[i] > 0){
+                finishPoss = i;
+                finishSize = arr[i];
+                tWater += (finishPoss-1) - startingPoss;
+                counter = 0;
+            }
+
             if(arr[i] != 0 && counter == 0){
                 startingPoss = i;
                 startingSize = arr[i];
                 counter++;
+                if(startingSize > 1){
+                    greaterThanOne = true;
+                }
             }
-            if(counter > 0 && arr[i] >= startingSize){
-                finishPoss = i;
-                finishSize = arr[i];
-                tWater = (finishPoss-1) - startingPoss;
-            } 
         }
         return tWater;
     }

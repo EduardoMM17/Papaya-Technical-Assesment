ff0a980afd6d5de06891f521e06578c5d191d153
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-02-10 22:33:27 -0500

    All the students are now in a vector, this makes it easier to manage the data

--- CC3.java
+++ CC3.java
@@ -5,22 +5,32 @@ public class CC3{
     public static void main(String[] args){   
         String fileName = "file.csv";
         File file = new File(fileName);
+        Vector<Student> students = new Vector<Student>();
         try{
             Scanner inputStream = new Scanner(file);
-            while (inputStream.hasNext()){
-                String data = inputStream.next();
-                System.out.println(data);
+            inputStream.nextLine();
+            while (inputStream.hasNextLine()){
+                Student student = new Student();
+                String data = inputStream.nextLine();
+                String[] values = data.split(",");
+                student.UserId = values[0];
+                student.FirstAndLastName = values[1];
+                String SVersion = values[2];
+                student.Version = Integer.parseInt(SVersion);
+                student.CourseMajor = values[3];
+                students.add(student);
             }
             inputStream.close();
         } catch (FileNotFoundException e){
             e.printStackTrace();
         }     
+        System.out.println(students.get(1).UserId);
     } 
 }
 
 class Student{
     String UserId;
-    String F&LName;
+    String FirstAndLastName;
     int Version;
     String CourseMajor;
 }
